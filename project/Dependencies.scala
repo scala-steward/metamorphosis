9d409b2609cb4fba93231e7c2344889c56c43a58
Scala Steward <scala_steward@virtuslab.com>
2022-09-11 15:23:07 +0000

    Update connect-transforms to 2.8.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Version {
     val scala = "2.12.16"
 
-    val kafka = "2.7.2"
+    val kafka = "2.8.2"
   }
 
   val kafkaConnectTransforms = "org.apache.kafka" % "connect-transforms" % Version.kafka
